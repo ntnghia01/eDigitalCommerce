1d577f2adec83a4f4a37666a9a81204c2b811f08
ntnghia01 <nguyentrungnghia26112001@gmail.com>
2024-07-06 10:07:56 +0700

    update: main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -17,13 +17,6 @@ import theme from './theme';
 //   </Provider>
 // )
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import store from './store';
-// import App from './App.jsx';
-// import './index.css';
-
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
